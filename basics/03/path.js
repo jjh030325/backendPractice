1227b74ce9f3eacdecfbbf916b4374adc69918cb
wpqlks7 <wpqlks7@gmail.com>
2024-07-17 01:59:22 +0900

    0717

--- basics/03/path.js
+++ basics/03/path.js
@@ -1,2 +1,17 @@
 // path 모듈 연습하기 ( 결과 비교 파일 : 03\results\path.js)
+const path = require("path");
 
+//join 전체 경로를 만듬
+const fullPath = path.join('some', 'work', 'ex.txt');
+console.log(fullPath);
+
+// 경로만 추출 = dirname
+const dir = path.dirname(fullPath);
+console.log(dir);
+
+// 파일 이름만 추출 = basename
+const fn1 = path.basename(__filename);
+console.log(`전체 경로(__filename): ${__filename}`);
+console.log(fn1);
+const fn2 = path.basename(__filename, '.js');
+console.log(fn2);
