9e972fa2e7cf8a07b59e50b5f4f585b4e837b1a6
wpqlks7 <wpqlks7@gmail.com>
2024-07-18 15:28:27 +0900

    0718 fs모듈 파일 입출력

--- basics/03/list-3.js
+++ basics/03/list-3.js
@@ -1,2 +1,16 @@
 // fs 모듈의 readdir 함수의 withFileTypes 옵션 연습하기 ( 결과 비교 파일 : 03\results\list-3.js)
 
+const fs = require("fs");
+
+fs.readFile("./example.txt", "utf8", (err, data) => {
+    if(err) {
+        console.log(err);
+    }
+    //console.log(data);
+    fs.writeFile("./test.txt", data, (err) => {
+        if(err) {
+            console.log(err);
+        }
+        console.log(`test.txt is saved.`);
+    });
+});
