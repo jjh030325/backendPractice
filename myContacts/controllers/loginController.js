1b7aad956c66b7eb3254be519d9c4a7fcda86ad2
wpqlks7 <wpqlks7@gmail.com>
2024-07-27 21:43:08 +0900

    0727 bcrypt, 쿠키

--- myContacts/controllers/loginController.js
+++ myContacts/controllers/loginController.js
@@ -1,18 +1,48 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+const jwtSecret = process.env.JWT_SECRET;
+const jwt = require("jsonwebtoken");
 
 const getLogin = (req, res) => {
     res.render("home");
 };
 
 const loginUser = asyncHandler(async(req, res) => {
-    const { username, password } = req.body;
-
-    if(username === "admin" && password === "1234") {
-        res.send("Login success");
-    } else {
-        res.send("Login Failed");
+    const { username, password} = req.body;
+    const user = await User.findOne({ username });
+    if(!user) {
+        return res.json( { message: '일치하는 사용자가 없습니다.'});
     }
 
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch) {
+        return res.json({ message: "비밀번호가 맞지 않습니다."});
+    }
+
+    const token = jwt.sign({ id:user._id}, jwtSecret);
+    res.cookie("token", token, {httpOnly: true});
+    res.redirect("/contacts");
+});
+
+// Register page
+// GET /register
+const getRegister = (req, res) => {
+    res.render("register");
+}
+
+// Register user
+// POST /register
+const registerUser = asyncHandler( async(req, res) => {
+    const { username, password, password2 } = req.body;
+    if (password === password2) {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({ username, password: hashedPassword });
+        res.json({ message: "Register successfule", user });
+    } else {
+        res.send("Register Failed");
+    }
 })
 
-module.exports = { getLogin, loginUser };
+module.exports = { getLogin, loginUser, getRegister, registerUser };
--- myContacts/routes/loginRoutes.js
+++ myContacts/routes/loginRoutes.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { getLogin, loginUser } = require("../controllers/loginController");
+const { 
+    getLogin,
+    loginUser, 
+    getRegister, 
+    registerUser 
+} = require("../controllers/loginController");
 
 router.route("/").get(getLogin).post(loginUser);
+router.route("/register").get(getRegister).post(registerUser);
 
 module.exports = router;
