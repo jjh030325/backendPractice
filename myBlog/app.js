3084ef2cdb1e61c2a358f83709cc99b9543fb598
wpqlks7 <wpqlks7@gmail.com>
2024-07-30 15:40:31 +0900

    0730 첫 화면 및 게시글 디비

--- myBlog/app.js
+++ myBlog/app.js
@@ -1,13 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const connectDb = require("./config/db");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+connectDb();
+
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
+app.use(express.static("public"));
 
 app.use("/", require("./routes/main"));
 
--- myBlog/routes/main.js
+++ myBlog/routes/main.js
@@ -1,18 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const mainLayout = "../views/layouts/main.ejs";
+const Post = require("../models/Post");
+const asyncHandler = require("express-async-handler");
 
-router.get(["/","/home"], (req, res) => {
+router.get(
+    ["/","/home"], 
+    asyncHandler(async(req, res) => {
     const locals = {
-        title: "Home"
+        title: "Home",
     }
-    res.render("index", { locals, layout: mainLayout});
-});
+    const data = await Post.find();
+    res.render("index", { locals, data, layout: mainLayout});
+})
+);
 router.get("/about", (req, res) => {
     const locals = {
         title: "About"
     }
     res.render("about", { locals, layout: mainLayout});
 });
+
+// 게시물 상세 보기
+// GET /post/:id
+router.get(
+    "/post/:id",
+    asyncHandler(async(req, res) => {
+        const data = await Post.findOne({ _id: req.params.id });
+        res.render("post", { data, layout: mainLayout});
+    })
+);
 
 module.exports = router;
