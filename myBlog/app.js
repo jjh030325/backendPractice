b95d2f9b754d4052d98aaa674e79734ab3141506
wpqlks7 <wpqlks7@gmail.com>
2024-08-01 02:38:42 +0900

    0801 관리자로그인로그아웃

--- myBlog/app.js
+++ myBlog/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const connectDb = require("./config/db");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,6 +14,11 @@ app.set("view engine", "ejs");
 app.set("views", "./views");
 
 app.use(express.static("public"));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(cookieParser());
 
 app.use("/", require("./routes/main"));
 app.use("/", require("./routes/admin"));
--- myBlog/routes/admin.js
+++ myBlog/routes/admin.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = express.Router();
 const adminLayout = "../views/layouts/admin";
 const adminLayout2 = "../views/layouts/admin-nologout";
+const asyncHandler = require("express-async-handler");
+const User = require("../models/User");
+const Post = require("../models/Post");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.JWT_SECRET;
 
 // Admin Page
 // GET /admin
@@ -11,5 +17,66 @@ router.get("/admin", (req, res) => {
     };
     res.render("admin/index", { locals, layout: adminLayout2 });
 });
+
+//check Login
+//POST /admin
+router.post("/admin", asyncHandler(async(req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne( { username });
+    if(!user) {
+        return res.status(401).json( { message: "일치하는 사용자가 없습니다."});
+    }
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if(!isValidPassword) {
+        return res.status(401).json( { message: "비밀번호가 일치하지 않습니다."});
+    }
+
+    const token = jwt.sign({ id:user._id }, jwtSecret);
+    res.cookie("token", token, { httpOnly: true});
+    res.redirect("/allPosts");
+})
+);
+
+
+// View Register Form
+// GET /register
+router.get("/register", asyncHandler(async(req, res) => {
+    res.render("admin/index", { layout: adminLayout2});
+}));
+
+// Register Adminstrator
+// POST /register
+router.post(
+    "/register", 
+    asyncHandler(async(req, res) => {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = await User.create( {
+            username: req.body.username,
+            password: hashedPassword
+        });
+        //res.json(`user created: ${user}`);
+    })
+);
+
+// Get all Posts
+// GET /allPosts
+router.get(
+    "/allPosts",
+    asyncHandler(async(req, res) => {
+        const locals = {
+            title: "Posts"
+        };
+        const data = await Post.find();
+        res.render("admin/allPosts", { locals, data, layout: adminLayout});
+    })
+);
+
+// Admin logout
+// GET /logout
+router.get("/logout", (req, res) => {
+    res.clearCookie("token");
+    res.redirect("/");
+})
 
 module.exports = router;
